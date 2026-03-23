a16d0bc0697d1a4f619e840025d267970e14a1cc
mohammad-ans <ansmuhammad340@gmail.com>
2026-03-23 19:39:54 +0500

    changed reqs origin

--- cpv/src/api.jsx
+++ cpv/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://yappyyap.xyz",
+  baseURL: "https://api.yappyyap.xyz",
   headers: {
     "Content-Type": "application/json",
   },
